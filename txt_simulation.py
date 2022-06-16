f99bfea3fcb6656f7fba4989a31eb3ca587fd198
kento-koyama <76421729+kento-koyama@users.noreply.github.com>
2022-06-16 22:06:24 +0900

    Update txt_simulation.py

--- txt_simulation.py
+++ txt_simulation.py
@@ -28,6 +28,5 @@ ax.tick_params(labelsize=16, direction='out')
 st.pyplot(fig)
     
 st.write('## Reference')
-st.write('Hiura, S., Abe, H., Koyama, K., Koseki, S. Transforming kinetic model into a stochastic inactivation model: Statistical evaluation of stochastic inactivation of individual cells in a bacterial population,  2020, Food Microbiology, 91, 103508.')
 link = '[doi.org/10.1016/j.fm.2020.103508](https://doi.org/10.1016/j.fm.2020.103508)'
-st.markdown(link, unsafe_allow_html=True)
+st.markdown('Hiura, S., Abe, H., Koyama, K., Koseki, S. Transforming kinetic model into a stochastic inactivation model: Statistical evaluation of stochastic inactivation of individual cells in a bacterial population,  2020, Food Microbiology, 91, 103508.',link, unsafe_allow_html=True)
